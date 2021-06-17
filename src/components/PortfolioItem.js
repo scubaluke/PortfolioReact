9a4abd034e12f262fa799703e873d469122a5d5e
Luke Gartland <lukegartland@gmail.com>
2021-06-17 08:55:43 -0500

    props update

--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import './PortfolioItem.css'
-
 import {Link} from 'react-router-dom'
+
 // import img from '../img/codeBackground.jpg'
 export default function PortfolioItem({to, img}) {
     return (
-     <Link className="portfolio__item" to="/meditation" className="portfolio__item">
+     <Link className="portfolio__item" to={to} className="portfolio__item">
         <img src={img} alt="" className="portfolio__img" />
      </Link>
     )
